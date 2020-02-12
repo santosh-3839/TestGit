1325fdb3b483adb1daed4351d3d5d42cefb0230f
santosh-3839 <santosh.3839@gmail.com>
2020-02-12 20:06:36 +0530

    Zuul microservices

---
+++ Zuul/src/test/java/com/spring/retail/Zuul/ZuulApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.spring.retail.Zuul;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class ZuulApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
